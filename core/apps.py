93a5c84391fc49b1754e839ab6b43fe52b0c2ffa
Diego Ceron <134111842+Zero100x@users.noreply.github.com>
2025-04-25 07:55:22 -0500

    Delete core directory

--- core/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'  # Asegúrate que coincida con el nombre de tu carpeta
